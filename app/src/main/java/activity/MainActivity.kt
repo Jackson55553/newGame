81833ed3bffacedae13dc77b5c946fa5d5a59da8
jacso <dudin10@mail.ru>
2021-08-21 19:42:31 +0300

    Initial commit

---
+++ app/src/main/java/activity/MainActivity.kt
@@ -0,0 +1,15 @@
+package activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import com.example.newgame.R
+import fragment.MainFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().replace(R.id.main_container, MainFragment()).commit()
+    }
+}
